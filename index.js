06e8ead9c46e912b632461b2097dd4e0300afd3f
Niklas Niemann Christensen <nnch.skp@edu.mercantec.dk>
2020-11-11 09:44:24 +0100

    Added database & created connection

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
+
 var http = require('http');
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "",
+  user: "",
+  password: "",
+  database: ""
+});
+
+con.connect(function(err) {
+  if(err) throw err;
+  console.log("Connected");
+});
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
